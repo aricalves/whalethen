1d7cf9386fb766ba703a3779db767293bc540450
Aric Alves <aric.alves@yahoo.com>
2017-12-02 11:07:28 -0800

    Add react-big-calendar, render spaghetti calendar

--- client/src/Calendar.jsx
+++ client/src/Calendar.jsx
@@ -1,24 +1,33 @@
 import React from 'react';
 import propTypes from 'prop-types';
+import Cal from 'react-big-calendar';
+import moment from 'moment';
+
+Cal.setLocalizer(Cal.momentLocalizer(moment));
 
 const Calendar = (props) => {
-  const setDate = () => {
-    const date = props.start.split('-');
-    date[2] = Number(date[2]) + props.i;
-    return date.join('-');
-  };
-
+  console.log(props);
   return (
     <div className="calendar">
-      <h1>{setDate()}</h1>
-      <div>index: {props.i}</div>
+      <Cal events={props.events[0].events} />
     </div>
   );
 };
 
 Calendar.propTypes = {
-  i: propTypes.number.isRequired,
-  start: propTypes.string.isRequired,
+
+};
+
+Calendar.propTypes = {
+  events: propTypes.instanceOf(Object).isRequired,
 };
 
 export default Calendar;
+
+/*
+    <div className="calendar">
+      <h1>{setDate()}</h1>
+      <div>index: {props.i}</div>
+    </div>
+
+*/
